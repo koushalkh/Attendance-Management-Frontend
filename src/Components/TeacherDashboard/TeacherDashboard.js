8051a5f2547fd18c0d20b73bd0f295d7418895db
koushal.k <koushal.k@hashedin.com>
2019-04-04 01:42:15 +0530

    added toast

--- src/Components/TeacherDashboard/TeacherDashboard.js
+++ src/Components/TeacherDashboard/TeacherDashboard.js
@@ -1,24 +1,48 @@
 import React, { Component } from "react";
 import "./TeacherDashboard.scss";
-
+import { SemanticToastContainer, toast } from "react-semantic-toasts";
+import "react-semantic-toasts/styles/react-semantic-alert.css";
 import { fetchStudents } from "../../actions/studentActions";
 import { connect } from "react-redux";
 import { Button, Icon, Label } from "semantic-ui-react";
 import Sidebar from "../Sidebar/Sidebar";
 import TeacherContent from "../TeacherContent/TeacherContent";
+import TakeAttandance from "../TakeAttandance/TakeAttandance";
 
 export default class TeacherDashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      takeAttandance: false
+    };
+  }
+  handleOnTakeAttandance = event => this.setState({ takeAttandance: true });
+  closeTakeAttandance = () => this.setState({ takeAttandance: false });
+  onTakeAttandance = event => {
+    toast({
+      type: "warning",
+      icon: "camera",
+      title: "SUCESS!!",
+      description: "Triggered attandance taking system",
+      animation: "bounce",
+      time: 5000
+    });
+    this.setState({ takeAttandance: false });
+  };
   render() {
     const { pendingRequests } = this.props;
+    const { takeAttandance } = this.state;
     return (
       <div className="teacher-dashboard-container">
         <Sidebar />
+        <SemanticToastContainer />;
         <div className="action-buttons">
           <Button
             icon
             labelPosition="left"
             // floated="left"
             className="action-button"
+            onClick={this.handleOnTakeAttandance}
           >
             <Icon name="play" />
             Take Attendance
@@ -40,6 +64,14 @@ export default class TeacherDashboard extends Component {
           </Button>
         </div>
         <TeacherContent />
+        {takeAttandance && (
+          <TakeAttandance
+            open={takeAttandance}
+            hasCurrentClass
+            onClose={this.closeTakeAttandance}
+            onTakeAttandance={this.onTakeAttandance}
+          />
+        )}
       </div>
     );
   }
